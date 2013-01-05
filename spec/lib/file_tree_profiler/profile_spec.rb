00d5fcbdafe4741ecf7564cc0e64f31fc75a8233
Jens Bissinger <mail@jens-bissinger.de>
2013-01-05 22:36:48 +0100

    spec different, partially different and equal profiles based on checksum

--- spec/lib/file_tree_profiler/profile_spec.rb
+++ spec/lib/file_tree_profiler/profile_spec.rb
@@ -16,4 +16,25 @@ describe FileTreeProfiler::Profile do
     its(:checksum) { should == profile.checksum }
     its(:size) { should == profile.size }
   end
+
+  context :equals do
+    let(:profile_a) { described_class.new(example_folder('equals', 'profile_a')) }
+    let(:profile_b) { described_class.new(example_folder('equals', 'profile_b')) }
+    subject { profile_a }
+    its(:checksum) { should == profile_b.checksum }
+  end
+
+  context :differents do
+    let(:profile_a) { described_class.new(example_folder('differents', 'profile_a')) }
+    let(:profile_b) { described_class.new(example_folder('differents', 'profile_b')) }
+    let(:profile_c) { described_class.new(example_folder('differents', 'profile_c')) }
+    subject { profile_a }
+    its(:checksum) { should_not == profile_b.checksum }
+    its(:checksum) { should_not == profile_c.checksum }
+
+    context :partially do
+      specify { profile_a.root.children.first.name.should == profile_c.root.children.first.name }
+      specify { profile_a.root.children.first.checksum.should == profile_c.root.children.first.checksum }
+    end
+  end
 end
